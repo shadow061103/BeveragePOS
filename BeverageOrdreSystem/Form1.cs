2804accd51a7b2a369047d474f8f23cd8140ee6d
shadow061103 <shadow061103@gmail.com>
2017-09-14 10:55:43 +0800

    add string
    add string

--- BeverageOrdreSystem/Form1.cs
+++ BeverageOrdreSystem/Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace BeverageOrdreSystem
 {
     public partial class Form1 : Form
-    {
+    {//hello
         public bool focus = true;
        Beveragefactory factory = new Beveragefactory();
       List<OrderDetail> order = new List<OrderDetail>();
